0b207adccd1d7ef00618f1a3cb801c982f41bc22
Michael Buch <mb2244@cl.cam.ac.uk>
2018-11-20 10:18:25 +0000

    separate sanitize from debug

--- report/src/fairness-dist.sh
+++ report/src/fairness-dist.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-THREADS_NUM=$1
-SHARED_SIZE=$2
-SINGLE_CORE=$3
-READ_WRITE=$4
-OUT_FILE="fairness${READ_ONLY}_${THREADS_NUM}_${SHARED_SIZE}_cores${SINGLE_CORE}.result"
+LOCK=$1
+THREADS_NUM=$2
+SHARED_SIZE=$3
+SINGLE_CORE=$4
+READ_WRITE=$5
+OUT_FILE="fairness${READ_ONLY}_${LOCK}_${THREADS_NUM}_${SHARED_SIZE}_cores${SINGLE_CORE}.result"
 echo "Writing results to file: ${OUT_FILE}"
 echo 0 0 > $OUT_FILE
-DEBUG=1 USE_FLAG_RW=1 make all
+eval "DEBUG=1 USE_${LOCK}=1 make all"
 ./harness.out $THREADS_NUM ${SHARED_SIZE} ${SINGLE_CORE} | grep "Called from:" >> $OUT_FILE 2>&1
 awk '{ print $3 }' ${OUT_FILE} | sort -n | uniq -c > "${OUT_FILE}.dist"
