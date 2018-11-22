34aaec80110c726fb5bbcc603dbda6655d67a21e
Michael Buch <michaelbuch12@gmail.com>
2018-11-22 17:35:02 +0000

    enhance scripts for write mode

--- report/src/run-array-lock.sh
+++ report/src/run-array-lock.sh
@@ -5,3 +5,5 @@ PREFIX='array-based'
 SUFFIX=$1
 ./run.sh ${PREFIX} 16 5 0 0 ${SUFFIX}
 ./run.sh ${PREFIX} 16 5000 0 0 ${SUFFIX}
+./run.sh ${PREFIX} 16 5 0 100 ${SUFFIX}
+./run.sh ${PREFIX} 16 5000 0 1000000 ${SUFFIX}
--- report/src/run-basic-mutex.sh
+++ report/src/run-basic-mutex.sh
@@ -5,5 +5,5 @@ PREFIX='mutex'
 SUFFIX=$1
 ./run.sh ${PREFIX} 16 5 0 0 ${SUFFIX}
 ./run.sh ${PREFIX} 16 5000 0 0 ${SUFFIX}
-./run.sh ${PREFIX} 16 5 100 0 ${SUFFIX}
-./run.sh ${PREFIX} 16 5000 1000000 0 ${SUFFIX}
+./run.sh ${PREFIX} 16 5 0 100 ${SUFFIX}
+./run.sh ${PREFIX} 16 5000 0 1000000 ${SUFFIX}
