7dae2bce250e9f9c9490f121fcd437c93a5d6ff5
Michael Buch <michaelbuch12@gmail.com>
2018-11-22 17:16:33 +0000

    use correct variable name in fairness script

--- report/src/fairness-dist.sh
+++ report/src/fairness-dist.sh
@@ -4,7 +4,7 @@ THREADS_NUM=$2
 SHARED_SIZE=$3
 SINGLE_CORE=$4
 READ_WRITE=$5
-OUT_FILE="fairness${READ_ONLY}_${LOCK}_${THREADS_NUM}_${SHARED_SIZE}_cores${SINGLE_CORE}.result"
+OUT_FILE="fairness${READ_WRITE}_${LOCK}_${THREADS_NUM}_${SHARED_SIZE}_cores${SINGLE_CORE}.result"
 echo "Writing results to file: ${OUT_FILE}"
 echo 0 0 > $OUT_FILE
 eval "DEBUG=1 USE_${LOCK}=1 make all"
