7db4030b811c5579d17d163d875842cd7bd8726d
Michael Buch <michaelbuch12@gmail.com>
2018-11-25 00:50:05 +0000

    clang format

--- report/src/harness.c
+++ report/src/harness.c
@@ -16,13 +16,13 @@ volatile sig_atomic_t global_tatas_lock = 0;
 volatile int* global_anderson_flags = NULL;
 
 volatile sig_atomic_t* g_ro_flags = NULL; // TODO: wrap flags in padded structs
-volatile sig_atomic_t g_rw_flag = 0;	  // Single writer
+volatile sig_atomic_t g_rw_flag   = 0;    // Single writer
 
-volatile sig_atomic_t global_anderson_tail = 0;
+volatile sig_atomic_t global_anderson_tail  = 0;
 volatile sig_atomic_t first_thread_complete = 0;
-volatile int operation_ctr =
-	0; // TODO: might need to move to thread_local or add synchronization
-	   // TODO: make sure it approximately causes correct number of writes
+volatile int operation_ctr
+    = 0; // TODO: might need to move to thread_local or add synchronization
+         // TODO: make sure it approximately causes correct number of writes
 
 typedef struct thread_info
 {
@@ -54,13 +54,15 @@ void log_thread( thread_info* ti )
 
 void eb_spin( int* ctr )
 {
-	if( ( *ctr )++ < 32 ) {
+	if( ( *ctr )++ < 32 )
+	{
 		struct timespec ts;
 		int milliseconds = 2 << *ctr;
-		ts.tv_sec = milliseconds / 1000;
-		ts.tv_nsec = ( milliseconds % 1000 ) * 1000000;
+		ts.tv_sec        = milliseconds / 1000;
+		ts.tv_nsec       = ( milliseconds % 1000 ) * 1000000;
 		nanosleep( &ts, NULL );
-	} else
+	}
+	else
 		pthread_yield();
 }
 
@@ -69,30 +71,37 @@ void cond_lock_ro( thread_info** ti )
 #ifdef USE_MUTEX
 	pthread_mutex_lock( &shared_arr_mutex );
 #elif USE_TATAS
-	do {
-		while( global_tatas_lock ) {
+	do
+	{
+		while( global_tatas_lock )
+		{
 			// spin
 			_mm_pause();
 		}
 	} while( __sync_lock_test_and_set( &global_tatas_lock, 1 ) );
 #elif USE_RW_TATAS
-	do {
+	do
+	{
 		int old = global_tatas_lock;
-		if( ( old >= 0 ) && ( __sync_bool_compare_and_swap( &global_tatas_lock,
-															old, old + 1 ) ) )
+		if( ( old >= 0 )
+		    && ( __sync_bool_compare_and_swap( &global_tatas_lock, old,
+		                                       old + 1 ) ) )
 			break;
 	} while( 1 );
 #elif USE_ANDERSON // flag-based reader-writer lock (array-based lock)
 	cond_lock_rw( ti );
 #elif USE_RW_ARRAY // http://joeduffyblog.com/2009/02/20/a-more-scalable-readerwriter-lock-and-a-bit-less-harsh-consideration-of-the-idea/
 	int backoff_ctr = 0;
-	while( 1 ) {
-		while( g_rw_flag == 1 ) {
+	while( 1 )
+	{
+		while( g_rw_flag == 1 )
+		{
 			eb_spin( &backoff_ctr );
 		}
 
 		__sync_fetch_and_add( &( g_ro_flags[( *ti )->thread_num] ), 1 );
-		if( g_rw_flag == 0 ) break;
+		if( g_rw_flag == 0 )
+			break;
 
 		__sync_fetch_and_add( &( g_ro_flags[( *ti )->thread_num] ), -1 );
 	}
@@ -119,16 +128,19 @@ void cond_lock_rw( thread_info** ti )
 #ifdef USE_MUTEX
 	pthread_mutex_lock( &shared_arr_mutex );
 #elif USE_TATAS
-	do {
-		while( global_tatas_lock ) {
+	do
+	{
+		while( global_tatas_lock )
+		{
 			// spin
 			_mm_pause();
 		}
 	} while( __sync_lock_test_and_set( &global_tatas_lock, 1 ) );
 #elif USE_RW_TATAS
-	do {
-		if( ( global_tatas_lock == 0 ) &&
-			__sync_bool_compare_and_swap( &global_tatas_lock, 0, -1 ) )
+	do
+	{
+		if( ( global_tatas_lock == 0 )
+		    && __sync_bool_compare_and_swap( &global_tatas_lock, 0, -1 ) )
 			break;
 	} while( 1 );
 #elif USE_ANDERSON // flag-based reader-writer lock (array-based lock)
@@ -136,17 +148,20 @@ void cond_lock_rw( thread_info** ti )
 	// https://geidav.wordpress.com/2016/12/03/scalable-spinlocks-1-array-based/
 	// https://www.csd.uoc.gr/~hy586/material/lectures/cs586-Section3.pdf
 	// Art of Multiprocessor Programming Ch. 7.5.1
-	int n = ( *ti )->number_of_threads;
+	int n        = ( *ti )->number_of_threads;
 	int old_tail = __sync_fetch_and_add( &global_anderson_tail, 1 * 4 );
 	int slot = ( *ti )->anderson_slot = ( ( old_tail + 1 * 4 ) ) % n;
-	while( !global_anderson_flags[slot] ) {
+	while( !global_anderson_flags[slot] )
+	{
 		// spin
 		_mm_pause();
 	}
 #elif USE_RW_ARRAY
 	int ctr = 0;
-	while( true ) {
-		if( g_rw_flag == 0 && __sync_lock_test_and_set( &g_rw_flag, 1 ) == 0 ) {
+	while( true )
+	{
+		if( g_rw_flag == 0 && __sync_lock_test_and_set( &g_rw_flag, 1 ) == 0 )
+		{
 			for( int i = 0; i < ( *ti )->number_of_threads; ++i )
 				while( g_ro_flags[i] != 0 )
 					eb_spin( &ctr );
@@ -166,13 +181,13 @@ void cond_unlock_rw( thread_info** ti )
 #elif USE_RW_TATAS
 	global_tatas_lock = 0;
 #elif USE_ANDERSON // flag-based reader-writer lock (array-based lock)
-	int n = ( *ti )->number_of_threads;
-	int slot = ( *ti )->anderson_slot;
-	global_anderson_flags[slot] = 0;
+	int n                                       = ( *ti )->number_of_threads;
+	int slot                                    = ( *ti )->anderson_slot;
+	global_anderson_flags[slot]                 = 0;
 	global_anderson_flags[( slot + 1 * 4 ) % n] = 1;
-#ifdef DEBUG
+#	ifdef DEBUG
 	printf( "Unlocking %d\n", ( *ti )->thread_num );
-#endif
+#	endif
 #elif USE_RW_ARRAY
 	__sync_lock_release( &g_rw_flag );
 #endif
@@ -180,8 +195,8 @@ void cond_unlock_rw( thread_info** ti )
 
 void lock( thread_info** ti )
 {
-	if( ( *ti )->ops_until_write != 0 &&
-		( *ti )->operation_ctr >= ( *ti )->ops_until_write )
+	if( ( *ti )->ops_until_write != 0
+	    && ( *ti )->operation_ctr >= ( *ti )->ops_until_write )
 		cond_lock_rw( ti );
 	else
 		cond_lock_ro( ti );
@@ -189,14 +204,16 @@ void lock( thread_info** ti )
 
 void unlock( thread_info** ti )
 {
-	if( ( *ti )->ops_until_write != 0 &&
-		( *ti )->operation_ctr >= ( *ti )->ops_until_write ) {
+	if( ( *ti )->ops_until_write != 0
+	    && ( *ti )->operation_ctr >= ( *ti )->ops_until_write )
+	{
 		cond_unlock_rw( ti );
 		( *ti )->operation_ctr = 0;
 #ifdef DEBUG
 		puts( "unlocking write" );
 #endif
-	} else
+	}
+	else
 		cond_unlock_ro( ti );
 }
 
@@ -206,8 +223,10 @@ delay( int d )
 {
 	int i, k;
 	int x = 0;
-	for( i = 0; i < d; i++ ) {
-		for( k = 0; k < 100000; k++ ) {
+	for( i = 0; i < d; i++ )
+	{
+		for( k = 0; k < 100000; k++ )
+		{
 			x += i + k;
 		}
 	}
@@ -233,9 +252,11 @@ static void* __attribute__( ( noinline ) ) __attribute__( ( optimize( "O0" ) ) )
 thread_sum_unlim_fn( void* arg )
 {
 	thread_info* t_info = arg;
-	if( t_info->run_on_single_core ) schedule_on_core();
+	if( t_info->run_on_single_core )
+		schedule_on_core();
 
-	while( 1 /*!first_thread_complete*/ ) {
+	while( 1 /*!first_thread_complete*/ )
+	{
 		lock( &t_info );
 		log_thread( t_info );
 		sum_array( *( t_info->shared_data ), t_info->shared_data_sz );
@@ -250,10 +271,12 @@ thread_sum_fn( void* arg )
 {
 	pthread_setcanceltype( PTHREAD_CANCEL_ASYNCHRONOUS, NULL );
 	thread_info* t_info = arg;
-	if( t_info->run_on_single_core ) schedule_on_core();
+	if( t_info->run_on_single_core )
+		schedule_on_core();
 
 	int i;
-	for( i = 0; i < t_info->thread_arg; ++i ) {
+	for( i = 0; i < t_info->thread_arg; ++i )
+	{
 		lock( &t_info );
 		log_thread( t_info );
 		sum_array( *( t_info->shared_data ), t_info->shared_data_sz );
@@ -269,7 +292,8 @@ sum_array( int* arr, size_t sz )
 {
 	int sum = 0;
 	int i;
-	for( i = 0; i < sz; ++i ) {
+	for( i = 0; i < sz; ++i )
+	{
 		sum += arr[i];
 	}
 }
@@ -289,51 +313,53 @@ int main( int argc, char** argv )
 	tinfo = calloc( num_threads, sizeof( thread_info ) );
 
 	int shared_data_sz = atoi( argv[2] );
-	int* shared_arr = malloc( shared_data_sz * sizeof( int ) );
+	int* shared_arr    = malloc( shared_data_sz * sizeof( int ) );
 	int j;
 	for( j = 0; j < shared_data_sz; ++j )
 		shared_arr[j] = rand() % ( shared_data_sz * 5 );
 
 	/* Initialize global anderson lock flags */
-	global_anderson_flags =
-		calloc( 4 * num_threads, sizeof( int ) ); // * 4 for padding
-	global_anderson_flags[0] = 1;				  // First flag available
+	global_anderson_flags
+	    = calloc( 4 * num_threads, sizeof( int ) ); // * 4 for padding
+	global_anderson_flags[0] = 1;                   // First flag available
 
 	/* Initialize rw queue lock flags */
 	g_ro_flags = calloc( num_threads, sizeof( int ) ); // Multiple readers
 
 	int i, tret;
 	void* res;
-	tinfo[0].thread_num = 0;
-	tinfo[0].thread_arg = SUM_LOOP_NUM;
+	tinfo[0].thread_num         = 0;
+	tinfo[0].thread_arg         = SUM_LOOP_NUM;
 	tinfo[0].run_on_single_core = atoi( argv[3] );
-	tinfo[0].shared_data = &shared_arr;
-	tinfo[0].shared_data_sz = shared_data_sz;
-	tinfo[0].anderson_slot = 0;
-	tinfo[0].number_of_threads = num_threads;
-	tinfo[0].ops_until_write = atoi( argv[4] );
-	tinfo[0].operation_ctr = 0;
+	tinfo[0].shared_data        = &shared_arr;
+	tinfo[0].shared_data_sz     = shared_data_sz;
+	tinfo[0].anderson_slot      = 0;
+	tinfo[0].number_of_threads  = num_threads;
+	tinfo[0].ops_until_write    = atoi( argv[4] );
+	tinfo[0].operation_ctr      = 0;
 	tret = pthread_create( &( tinfo[0].thread_id ), NULL, &thread_sum_fn,
-						   &tinfo[0] );
+	                       &tinfo[0] );
 
-	for( i = 1; i < num_threads; ++i ) {
-		tinfo[i].thread_num = i;
-		tinfo[i].thread_arg = SUM_LOOP_NUM;
+	for( i = 1; i < num_threads; ++i )
+	{
+		tinfo[i].thread_num         = i;
+		tinfo[i].thread_arg         = SUM_LOOP_NUM;
 		tinfo[i].run_on_single_core = atoi( argv[3] );
-		tinfo[i].shared_data = &shared_arr;
-		tinfo[i].number_of_threads = num_threads;
-		tinfo[i].shared_data_sz = shared_data_sz;
-		tinfo[i].anderson_slot = 0;
-		tinfo[i].ops_until_write = atoi( argv[4] );
-		tinfo[i].operation_ctr = 0;
+		tinfo[i].shared_data        = &shared_arr;
+		tinfo[i].number_of_threads  = num_threads;
+		tinfo[i].shared_data_sz     = shared_data_sz;
+		tinfo[i].anderson_slot      = 0;
+		tinfo[i].ops_until_write    = atoi( argv[4] );
+		tinfo[i].operation_ctr      = 0;
 		tret = pthread_create( &( tinfo[i].thread_id ), NULL,
-							   &thread_sum_unlim_fn, &tinfo[i] );
+		                       &thread_sum_unlim_fn, &tinfo[i] );
 	}
 
 	tret = pthread_join( tinfo[0].thread_id, &res );
 
 	// TODO: add cleanup handler to avoid leaks (flagged by tsan)
-	for( i = 1; i < num_threads; ++i ) {
+	for( i = 1; i < num_threads; ++i )
+	{
 		pthread_cancel( tinfo[i].thread_id );
 	}
 
