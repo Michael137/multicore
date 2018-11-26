6a403b5d81c26eb0d30832a05bb02f516cc6d074
Michael Buch <michaelbuch12@gmail.com>
2018-11-26 00:39:11 +0000

    dont overwrite report

--- report/src/benchmark.sh
+++ report/src/benchmark.sh
@@ -10,13 +10,14 @@ SHARED_SIZE=$3
 SINGLE_CORE=$4
 READ_WRITE=$5
 OUT_FILE="${PREFIX}_harness${READ_WRITE}_${THREADS_NUM}_${SHARED_SIZE}_cores${SINGLE_CORE}.result"
-echo "Writing results to file: ${OUT_FILE}"
-results=()
 
+echo "Writing results to file: ${OUT_FILE}"
+
+results=()
+truncate -s 0 ${OUT_FILE}.report
 for i in `seq 1 $THREADS_NUM`
 do
 
-	truncate -s 0 ${OUT_FILE}.report
 	# Run perf stat 25 times
 	# TODO: add "-x \;" option?
 	# perf stat --pre 'sleep 2' -r 10 -d -d \
@@ -28,6 +29,8 @@ do
 			| tee -a ${OUT_FILE}.report \
 			| grep "seconds time elapsed" | awk '{print $1" "$7}')
 	)
+
+	sleep 5
 done
 
 truncate -s 0 ${OUT_FILE}
